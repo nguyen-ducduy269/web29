98eae7feb705462220e75463bd42107158f5e8c3
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2022-11-20 10:58:33 +0700

    nội dung bài học

--- project-web-tĩnh/assete/js/custom.js
+++ project-web-tĩnh/assete/js/custom.js
@@ -1,3 +1,41 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel();
+//Banner
+$("#banner").owlCarousel({
+  loop: true, // vòng lặp các phần tử
+  margin: 10,
+  nav: false, // navigation
+  dots: false, // chấm tròn chuyển trang
+  autoplay: true,
+  autoplayTimeout: 3000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
+//Limit Time Offer
+$("#limit-time").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
 });
