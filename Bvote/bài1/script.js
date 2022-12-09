33e8231cd6067f68716c38666215f2f986a0e72c
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2022-12-09 10:18:50 +0700

    nội dung bài học

--- Bvote/bài1/script.js
+++ Bvote/bài1/script.js
@@ -24,45 +24,16 @@ $(".owl-carousel").owlCarousel({
   margin: 10,
   nav: true,
   autoplay: true,
-  autoplayTimeout: 7000,
+  autoplayTimeout: 10000,
   responsive: {
     0: {
       items: 1,
     },
     600: {
-      items: 1,
+      items: 3,
     },
     1000: {
-      items: 2,
+      items: 5,
     },
   },
 });
-
-// $("#layer-owl").owlCarousel({
-//   navText: [
-//     // button nav
-//     `<span aria-label= "Previous">
-//         <i class="fa-solid fa-chevron-left"></i>
-//       </span>`,
-
-//     `<span aria-label= "Next">
-//         <i class="fa-solid fa-chevron-right"></i>
-//       </span>`,
-//   ],
-//   loop: true,
-//   margin: 10,
-//   nav: true,
-//   autoplay: true,
-//   autoplayTimeout: 10000,
-//   responsive: {
-//     0: {
-//       items: 1,
-//     },
-//     600: {
-//       items: 3,
-//     },
-//     1000: {
-//       items: 5,
-//     },
-//   },
-// });
