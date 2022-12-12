070c767816eba21c64a407160d7f29480d5d2dc7
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2022-12-12 10:27:27 +0700

    nội dung bài học

--- project-web-tĩnh/assete/js/vuighe.js
+++ project-web-tĩnh/assete/js/vuighe.js
@@ -8,7 +8,18 @@ $(".close-btn").on("click", function () {
   x.css("left", "-448px");
 });
 
-$(".owl-carousel").owlCarousel({
+// Owl Carousel
+$("#first-banner").owlCarousel({
+  navText: [
+    // button nav
+    `<span aria-label= "Previous">
+        <i class="fa-solid fa-chevron-left"></i>
+      </span>`,
+
+    `<span aria-label= "Next">
+        <i class="fa-solid fa-chevron-right"></i>
+      </span>`,
+  ],
   loop: true,
   margin: 10,
   nav: true,
@@ -19,10 +30,30 @@ $(".owl-carousel").owlCarousel({
       items: 1,
     },
     600: {
-      items: 3,
+      items: 1,
     },
     1000: {
-      items: 5,
+      items: 1,
+    },
+  },
+});
+
+$("#second-banner").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
     },
   },
 });
