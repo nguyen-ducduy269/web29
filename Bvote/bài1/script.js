54c3e66622258bfaa7d8157f194036172fba74cf
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2022-12-14 10:43:16 +0700

    nội dung bài học

--- Bvote/bài1/script.js
+++ Bvote/bài1/script.js
@@ -9,7 +9,27 @@ $(".close-btn").on("click", function () {
 });
 
 // Owlcarousel
-$(".owl-carousel").owlCarousel({
+$("#middle-image").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 10000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+  },
+});
+
+$("#layer").owlCarousel({
   navText: [
     // button nav
     `<span class="owl-prev">
