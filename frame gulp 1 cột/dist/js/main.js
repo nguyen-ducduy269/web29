c3b2feb1e0a0e4068c9221f539f8b15b3209c41d
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-02-01 09:46:01 +0700

    nội dung bài học

--- frame gulp 1 cột/dist/js/main.js
+++ frame gulp 1 cột/dist/js/main.js
@@ -1,49 +1,50 @@
 $(document).ready(function () {
-    AOS.init({
-        once: true,
-        offset: 0,
-        easing: 'ease-in-out-cubic',
-        duration: "300",
-    });
+  AOS.init({
+    once: true,
+    offset: 0,
+    easing: "ease-in-out-cubic",
+    duration: "300",
+  });
 
+  if ($(this).scrollTop() >= 42) {
+    $(".header-nav").addClass("scrolled");
+  } else {
+    $(".header-nav").removeClass("scrolled");
+  }
+  $(window).on("load", function () {
     if ($(this).scrollTop() >= 42) {
-        $(".header-nav").addClass("scrolled");
+      $(".header-nav").addClass("scrolled");
     } else {
-        $(".header-nav").removeClass("scrolled");
+      $(".header-nav").removeClass("scrolled");
     }
-    $(window).on("load", function () {
-        if ($(this).scrollTop() >= 42) {
-            $(".header-nav").addClass("scrolled");
+  });
+  $(document).scroll(function () {
+    if ($(this).scrollTop() >= $("#main").offset().top) {
+      $(".back-top").addClass("active");
+    } else {
+      $(".back-top").removeClass("active");
+    }
+    if ($(this).scrollTop() >= 42) {
+      $("#header").addClass("scrolled");
+    } else {
+      $("#header").removeClass("scrolled");
+    }
+  });
+  $(".back-top").on("click", function () {
+    $(".back-top").removeClass("active");
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
+});
 
-
-        } else {
-            $(".header-nav").removeClass("scrolled");
-
-        }
-    });
-    $(document).scroll(function () {
-        if ($(this).scrollTop() >= $("#main").offset().top) {
-            $(".back-top").addClass("active");
-
-        } else {
-            $(".back-top").removeClass("active");
-
-        }
-        if ($(this).scrollTop() >= 42) {
-            $("#header").addClass("scrolled");
-
-
-        } else {
-            $("#header").removeClass("scrolled");
-
-        }
-    });
-    $(".back-top").on("click", function () {
-        $(".back-top").removeClass("active");
-        $("html, body").animate({
-            scrollTop: 0
-        }, 1000);
-    });
-
-
+let x = $(".menu-respon");
+$(".show__menu").on("click", function () {
+  x.css("left", "0");
+});
+$(".close__menu").on("click", function () {
+  x.css("left", "-250px");
 });
