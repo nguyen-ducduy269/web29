71c3527fe2a67807143e3e0c2137e655a3c246da
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-02-14 11:07:36 +0700

    nội dung bài học

--- frame gulp 1 cột/app/js/main.js
+++ frame gulp 1 cột/app/js/main.js
@@ -49,4 +49,19 @@ $(".close__menu").on("click", function () {
   x.css("left", "-250px");
 });
 
-
+$("#footer-details").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
