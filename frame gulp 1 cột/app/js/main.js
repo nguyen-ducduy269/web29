3e88bd5e398933041fb1700de1c7836628fadaaf
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-02-20 09:38:45 +0700

    nội dung bài học

--- frame gulp 1 cột/app/js/main.js
+++ frame gulp 1 cột/app/js/main.js
@@ -54,14 +54,20 @@ $("#footer-details").owlCarousel({
   margin: 10,
   nav: true,
   responsive: {
-    0: {
-      items: 1,
+    320: {
+      items: 3,
+    },
+    375: {
+      items: 2,
     },
     600: {
       items: 3,
     },
     1000: {
-      items: 5,
+      items: 3,
+    },
+    1200: {
+      items: 3,
     },
   },
 });
