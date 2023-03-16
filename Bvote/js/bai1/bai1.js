060509bb476ed15bca1554b11fa3354325f0efb3
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-03-16 11:29:32 +0700

    nội dung bài học

--- Bvote/js/bai1/bai1.js
+++ Bvote/js/bai1/bai1.js
@@ -3,24 +3,41 @@
 // b) Có các nút nhân đôi, thêm, sửa, xóa 1 phần tử mảng thông qua các nút button trên màn hình
 // c) nút sắp xếp các phần tử trong mảng theo chiều tăng dần, giảm dần.
 // d) Khi ấn vào các button sẽ hiển thị và nhập yêu cầu thông qua câu lệnh prompt(), sau mỗi lần thay đổi sẽ có 1 dòng chữ thông báo và hiển thị mảng mới sau khi thay đổi.
-const list = [];
 
-function abc() {
+// const list = [];
+// tạo danh sách các giá trị đã nhập
+// const ul = document.getElementById("abc");
+const ul = document.createElement("abc");
+document.body.appendChild(ul);
+ul.innerHTML = list = [];
+const newList = list.concat(list);
+
+function add() {
   // giá trị của value được nhận từ prompt nên không cần truyền vào hàm
   const value = prompt("Nhập họ tên của bạn:");
   if (list.length >= 10 || value === null || value === "") {
     // nếu độ dài nhập sai thì hiển thị giá trị không hợp lệ
-    document.querySelector(".button").innerHTML = "Giá trị nhập không hợp lệ";
+    document.querySelector("abc").innerHTML = "Giá trị nhập không hợp lệ";
   } else {
     // nếu độ dài nhập hợp lệ thì thêm phần tử nhập vào trong mảng
     list.push(value);
     console.log(value);
-    // tạo danh sách các giá trị đã nhập
-    const ul = document.getElementById("abc");
     // ul.innerHTML = list.join("<br>");
-    // thêm giá trị nhập vào danh sách
+    // thêm phần tử bên trong danh sách
     const li = document.createElement("li");
+    // Nội dung nhập vào đc gán vào phần tử trong danh sách
     li.textContent = value;
+    // thêm phần tử đã gán vào trong danh sách
     ul.appendChild(li);
   }
+}
+
+function deleteItem() {
+  const li = document.querySelector("li");
+  // xóa phần tử khỏi list
+  ul.removeChild(li);
+}
+
+function dbItem() {
+  console.log(newList);
 }
