f1bc73532ebe9f70be775b1cbecee9d8ac5f9335
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-03-29 11:31:08 +0700

    up

--- Bvote/js/bai2/script.js
+++ Bvote/js/bai2/script.js
@@ -6,48 +6,50 @@
 // a)	Khi ấn vào các button sẽ hiển thị và nhập yêu cầu thông qua câu lệnh prompt(), sau mỗi lần thay đổi sẽ có 1 dòng chữ thông báo và hiển thị mảng mới sau khi thay đổi.
 
 function bai2() {
-  document.body.appendChild(btnAddItem);
+  //   document.body.appendChild(btnAddItem);
   document.body.appendChild(btnFootageItem);
   show(list);
 }
 
 const demo = document.getElementById("demo");
-const list = "5, 2, 6, 3, 1, 9";
+const list = "5 2 6 3 1 9";
+console.log(list.length);
+console.log(list.indexOf("6"));
 // chuyển từ dạng chuỗi về dạng mảng
-const newList = list.split(", ");
-console.log("newList", newList);
-let value;
+// const newList = list.split("   ");
+// console.log("newList", newList);
+// let value;
 
 function show(string) {
   // thực hiện ghép chuỗi
-  demo.innerHTML = string.concat(" ");
+  demo.innerHTML = string.concat("");
 }
-
-const btnAddItem = document.createElement("button");
-btnAddItem.innerHTML = "Nhập";
-btnAddItem.classList.add("btn");
-btnAddItem.setAttribute("onclick", "addFunc()");
 
 const btnFootageItem = document.createElement("button");
 btnFootageItem.innerHTML = "Chiều dài chuỗi mới nhập";
 btnFootageItem.classList.add("btn");
 btnFootageItem.setAttribute("onclick", "footFunc()");
 
-function addFunc() {
-  const value = prompt("Nhập vào chuỗi: ");
-  if (value === null || value === "" || value.length < 10) {
-    demo.innerHTML = "Giá trị nhập vào không hợp lệ";
-  } else {
-    // thực hiện phương thức thêm vào như 1 mảng
-    newList.push(value);
-    show(newList);
-  }
+function footFunc(list) {
+  // truy cập vào phần tử cuối cùng của chuỗi
+  const listLength = list[list.length - 1];
+  console.log("lastlistindex", listLength);
+  show(listLength.length);
 }
 
-function footFunc(newList) {
-  for (let i = 0; i > newList.length; i++) {
-    newList.length += i;
-    demo.innerHTML = newList.length;
-  }
-  return "Độ dài của chuỗi mới nhập là" + newList.length;
-}
+// const btnAddItem = document.createElement("button");
+// btnAddItem.innerHTML = "Nhập";
+// btnAddItem.classList.add("btn");
+// btnAddItem.setAttribute("onclick", "addFunc()");
+
+// function addFunc() {
+//   const value = prompt("Nhập vào chuỗi: ");
+//   if (value === null || value === "" || value.length < 10) {
+//     demo.innerHTML = "Giá trị nhập vào không hợp lệ";
+//   } else {
+//     // thực hiện phương thức thêm vào như 1 mảng
+//     newList.push(value);
+//     list = newList.toString();
+//     show(list);
+//   }
+// }
