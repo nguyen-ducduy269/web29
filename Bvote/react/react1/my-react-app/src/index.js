0dd36cc9958bd24e6647b6a5c2ca6fcfcec23468
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-04-24 11:41:32 +0700

    up

--- Bvote/react/react1/my-react-app/src/index.js
+++ Bvote/react/react1/my-react-app/src/index.js
@@ -1,23 +1,89 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const Hobby = () => {
   return (
-    // chỉ clone đc HTMl
-    <div class="menu-list">
-      <a href="/" class="menu-item">
-        Trang chủ
-      </a>
-      <a href="/shop" class="menu-item">
-        Cửa hàng
-      </a>
-      <a href="/about" class="menu-item">
-        Về chúng tôi
-      </a>
-      <a href="/contact" class="menu-item">
-        Liên hệ
-      </a>
-    </div>
+    <>
+      <h1 className="header">Quản lý công việc</h1>
+      <div className="container">
+        <div className="left-job">
+          <p className="add">Thêm công việc</p>
+
+          <div className="form">
+            <label>Tên: </label>
+            <br></br>
+            <input className="name" type="text"></input>
+            <br></br>
+            <label>Trạng thái: </label>
+            <select>
+              <option>Kích hoạt</option>
+              <option>Ẩn</option>
+            </select>
+
+            <div className="button">
+              <button className="btn_add">Thêm</button>
+              <button className="btn_remove">Hủy bỏ</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="right-job">
+          <button>Thêm công việc</button>
+          <div className="more">
+            <input
+              className="main-input"
+              type="text"
+              placeholder="Nhập từ khóa..."
+            ></input>
+            <button>Tìm</button>
+            <button className="reduce">Sắp xếp</button>
+          </div>
+
+          <table>
+            <thead>
+              <tr>
+                <td className="stt">STT</td>
+                <td className="name">Tên</td>
+                <td className="status">Trạng thái</td>
+                <td className="activity">Hành động</td>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td className="stt"></td>
+                <td className="name">
+                  <input className="add-input" type="text"></input>
+                </td>
+                <td className="status">
+                  <select>
+                    <option>Tất cả</option>
+                    <option>Kích hoạt</option>
+                    <option>Ẩn</option>
+                  </select>
+                </td>
+                <td className="activity"></td>
+              </tr>
+
+              <tr>
+                <td className="stt">1</td>
+                <td className="name">Học lập trình</td>
+                <td className="status">
+                  <p>Kích hoạt</p>
+                </td>
+                <td className="activity">
+                  <div className="button">
+                    <button className="btn_add">Sửa</button>
+                    <button className="btn_remove">Xóa</button>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 };
 
