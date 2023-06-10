95c711ba0157c517531aceeaa2d26ba526620ac6
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-06-10 11:31:57 +0700

    up

---
+++ javascript/newproject/js/data.js
@@ -0,0 +1,55 @@
+const clothes = [
+  {
+    name: "Women's Fashion",
+    image: "./img/women-clothes.png",
+  },
+  {
+    name: "Men's Fashion",
+    image: "./img/men-clothes.png",
+  },
+  {
+    name: "Phones & Telecommunications",
+    image: "./img/phone.png",
+  },
+  {
+    name: "Computer, Office & Security",
+    image: "./img/computer.png",
+  },
+  {
+    name: "Consumer Electronics",
+    image: "./img/electronic.png",
+  },
+  {
+    name: "Jewelry & Watches",
+    image: "./img/jewelry.png",
+  },
+  {
+    name: "Home, Pet & Appliances",
+    image: "./img/home.png",
+  },
+  {
+    name: "Bags & Shoes",
+    image: "./img/bags.png",
+  },
+  {
+    name: "Toys, Kids & Babies",
+    image: "./img/toys.png",
+  },
+  {
+    name: "Outdoor Fun & Sports",
+    image: "./img/sports.png",
+  },
+  {
+    name: "Beauty, Health & Hair",
+    image: "./img/beauty.png",
+  },
+  {
+    name: "Automobiles & Motorcycles",
+    image: "./img/motorcycles.png",
+  },
+  {
+    name: "Tools & Home Improvement",
+    image: "./img/improvement.png",
+  },
+];
+export default clothes;
---
+++ javascript/newproject/js/script.js
@@ -0,0 +1,22 @@
+import clothes from "./data.js";
+
+function renderCategories() {
+  clothes.map((product) => {
+    const listCate = document.querySelector(".list-categories");
+    let itemCate = document.createElement("a");
+    itemCate.classList.add("item-cate");
+
+    let itemImg = document.createElement("div");
+    itemImg.classList.add("img");
+    itemImg.style.backgroundImage = `url(${product.image})`;
+
+    let itemDetail = document.createElement("div");
+    itemDetail.classList.add("detail");
+    itemDetail.innerHTML = product.name;
+
+    itemCate.appendChild(itemImg);
+    itemCate.appendChild(itemDetail);
+    listCate.appendChild(itemCate);
+  });
+}
+renderCategories();
