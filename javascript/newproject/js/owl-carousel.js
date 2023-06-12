48f6cf53cab49bda4c69054d67c714a51f320dc1
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-06-12 11:34:57 +0700

    up

--- javascript/newproject/js/owl-carousel.js
+++ javascript/newproject/js/owl-carousel.js
@@ -2,20 +2,72 @@ $(document).ready(function () {
   $(".owl-carousel").owlCarousel();
 });
 
-$(".owl-carousel").owlCarousel({
+$(".big-one").owlCarousel({
   loop: true,
   Infinity: true,
   margin: 10,
   nav: true,
+  autoplay: true,
+  autoplayTimeout: 10000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    320: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
+$(".slick-track").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    320: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    800: {
+      items: 2,
+    },
+    1000: {
+      items: 4,
+    },
+    1200: {
+      items: 4,
+    },
+  },
+});
+
+$(".new-deals").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 5000,
   responsive: {
     0: {
       items: 1,
     },
     600: {
-      items: 3,
+      items: 5,
     },
     1000: {
-      items: 1,
+      items: 2,
     },
   },
 });
