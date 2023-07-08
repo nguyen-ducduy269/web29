ed6c39d41605d875d1a4afe9f35eb81beb315cbc
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-07-08 11:44:15 +0700

    up

--- Bytesoft/jquery/bài 1/script.js
+++ Bytesoft/jquery/bài 1/script.js
@@ -1,3 +1,4 @@
+// Tạo bàn cờ caro có các hiệu ứng: xuất hiện, biến mất, toggle và thực hiện đánh X,O xen kẽ.
 $(".hide").click(() => {
   $("table").hide();
 });
@@ -20,10 +21,55 @@ $("td").click((e) => {
 });
 
 // Tìm tất cả các thẻ <input type=’checkbox’> và hiển thị giá trị đang chọn (checked) trong các thẻ checkbox.
-$(document).ready(() => {
-  $('input[type="checkbox"]').each(function () {
-    if ($(this).is(":checked")) {
-      $("#show").append($(this).val());
-    }
+$(document).ready(function () {
+  var result = "";
+  $("#show").click(function () {
+    $("input:checkbox").each(function () {
+      if ($(this).is(":checked")) {
+        result += $(this).val();
+      }
+    });
+    alert("Giá trị đang chọn là: " + result);
+  });
+});
+
+// Tìm tất cả các thẻ <option> của thẻ <select id=”country”>
+$(document).ready(function () {
+  let option = "";
+  const select = $("option");
+  $(".country").click(function () {
+    $("option").each(function () {
+      option += $(this).val();
+    });
+    alert(
+      "Thẻ select có số lượng thẻ option là: " +
+        select.length +
+        " và có giá trị là: " +
+        option
+    );
+  });
+});
+
+// Tìm các thẻ <img> đặt trong <div id=”menu”>
+$(document).ready(function () {
+  let count = 0;
+  $("#image").click(function () {
+    $("#menu")
+      .children()
+      .each(function () {
+        count += 1;
+      });
+    alert("Số lượng thẻ img trong đặt trong <div id=”menu”> là: " + count);
+  });
+});
+
+// Tìm tất cả các thẻ <input type=’text’> chưa nhập dữ liệu. Định nghĩa và gắn class error cho thẻ đó.
+$(document).ready(function () {
+  $("#text-btn").click(function () {
+    $('input[type="text"]').each(function () {
+      if ($(this).val() === "") {
+        $(this).addClass("error");
+      }
+    });
   });
 });
