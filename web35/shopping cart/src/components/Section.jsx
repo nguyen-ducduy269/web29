ca46cecdccdd9801efe81397b2a0a5d019e8f040
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-09-11 18:34:48 +0700

    .

--- web35/shopping cart/src/components/Section.jsx
+++ web35/shopping cart/src/components/Section.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import Card from "./Card";
-const Section = ({ products }) => {
-  console.log("products", products);
-
-  const Cards = products.map((product) => (
-    <div className="col mb-5">
-      <Card
-        img={product.thumbnail}
-        title={product.title}
-        price={product.price}
-        pricesale={product.discountPercentage}
-        vote={product.rating}
-      />
-    </div>
-  ));
+const Section = ({ products, loading, error }) => {
   return (
     <section className="py-5">
       <div className="container px-4 px-lg-5 mt-5">
         <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-          {Cards}
+          {loading ? (
+            <div>Loading...</div>
+          ) : error ? (
+            <div>Lỗi khi tải dữ liệu</div>
+          ) : (
+            products.map((product) => (
+              <div className="col mb-5">
+                <Card
+                  img={product.thumbnail}
+                  title={product.title}
+                  price={product.price}
+                  pricesale={product.discountPercentage}
+                  vote={product.rating}
+                />
+              </div>
+            ))
+          )}
         </div>
       </div>
     </section>
