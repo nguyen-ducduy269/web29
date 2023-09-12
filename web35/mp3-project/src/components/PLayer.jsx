0154a62d218bd2219c726d32c129cadcde87cd10
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-09-12 17:13:51 +0700

    .

--- web35/mp3-project/src/components/PLayer.jsx
+++ web35/mp3-project/src/components/PLayer.jsx
@@ -1,54 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BsPlayFill,
   BsSkipEndFill,
   BsPauseFill,
   BsSkipStartFill,
+  BsRepeat,
+  BsShuffle,
+  BsHeart,
+  BsFillVolumeUpFill,
+  BsHeartFill,
 } from "react-icons/bs";
 
-const PLayer = ({ isPlaying, onToggle, onPrev, onNext }) => {
+const PLayer = ({ isPlaying, onToggle, onPrev, onNext, currentSong, song }) => {
+  const [fillHeart, setFillHeart] = useState(false);
+
   return (
     <>
-      <div
-        className="music"
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr",
-          height: 88,
-        }}
-      >
-        <div className="music-infor"></div>
+      <div className="music">
+        <div className="music-infor">
+          <div className="music-infor-img">
+            {currentSong ? (
+              <img src={currentSong.cover} alt="" />
+            ) : (
+              <img src={song[0].cover} alt="" />
+            )}
+          </div>
 
-        <div className="music-player" style={{ height: "50%" }}>
+          <div className="music-infor-des">
+            {currentSong ? (
+              <div className="music-name">{currentSong.title}</div>
+            ) : (
+              <div className="music-name">{song[0].title}</div>
+            )}
+
+            {currentSong ? (
+              <div className="music-author">{currentSong.owner.name}</div>
+            ) : (
+              <div className="music-author">{song[0].owner.name}</div>
+            )}
+          </div>
+
           <div
-            className="music-player-button"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              height: "100%",
-              alignItems: "center",
-            }}
+            className="music-infor-favorite"
+            onClick={() => setFillHeart(!fillHeart)}
           >
+            {fillHeart ? (
+              <>
+                <BsHeartFill style={{ color: "red" }} />
+              </>
+            ) : (
+              <>
+                <BsHeart />
+              </>
+            )}
+          </div>
+        </div>
+
+        <div className="music-player">
+          <div className="music-player-button">
+            <button>
+              <BsShuffle
+                style={{ fontSize: 12, color: "white", fontSize: "22px" }}
+              />
+            </button>
+
             <button onClick={() => onPrev()}>
-              <BsSkipStartFill style={{ fontSize: 16 }} />
+              <BsSkipStartFill
+                style={{ fontSize: 16, color: "white", fontSize: "22px" }}
+              />
             </button>
 
             <button onClick={() => onToggle()}>
               {!isPlaying ? (
-                <BsPlayFill style={{ fontSize: 16 }} />
+                <BsPlayFill
+                  style={{ fontSize: 16, color: "white", fontSize: "38px" }}
+                />
               ) : (
-                <BsPauseFill style={{ fontSize: 16 }} />
+                <BsPauseFill
+                  style={{ fontSize: 16, color: "white", fontSize: "38px" }}
+                />
               )}
             </button>
 
             <button onClick={() => onNext()}>
-              <BsSkipEndFill style={{ fontSize: 16 }} />
+              <BsSkipEndFill
+                style={{ fontSize: 16, color: "white", fontSize: "22px" }}
+              />
+            </button>
+
+            <button>
+              <BsRepeat
+                style={{ fontSize: 12, color: "white", fontSize: "22px" }}
+              />
             </button>
           </div>
-          <div className="music-player-time"></div>
+
+          <div className="music-player-time">
+            <div className="time-start">00:00</div>
+
+            <div className="time-duration">
+              <input type="range" name="" id="" max={257} min={0} />
+              <div className="progress"></div>
+            </div>
+
+            <div className="time-end">04.17</div>
+          </div>
         </div>
 
-        <div className="music-volumn"></div>
+        <div className="music-volumn">
+          <BsFillVolumeUpFill style={{ color: "white", fontSize: "20px" }} />
+          <div className="volume-duration">
+            <input type="range" name="" id="" max={1} min={0} />
+            <div className="volume-progress"></div>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ web35/mp3-project/src/components/PlayMusic.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const PlayMusic = ({ currentSong, song }) => {
+  return (
+    <>
+      <div className="play-music">
+        <div className="music-now">
+          <h4>
+            <b>Playing</b>
+          </h4>
+          {currentSong ? (
+            <div className="music-name">{currentSong.title}</div>
+          ) : (
+            <div className="music-name">{song[0].title}</div>
+          )}
+
+          {currentSong ? (
+            <div className="music-artist">{currentSong.owner.name}</div>
+          ) : (
+            <div className="music-artist">{song[0].owner.name}</div>
+          )}
+
+          {currentSong ? (
+            <div className="music-image">
+              <img src={currentSong.cover} />
+            </div>
+          ) : (
+            <div className="music-image">
+              <img src={song[0].cover} />
+            </div>
+          )}
+        </div>
+
+        <div className="music-list">
+          <div className="table">
+            <div style={{ display: "flex" }}>
+              <div className="number">#</div>
+              <div className="title">Title</div>
+              <div className="artist">Artist</div>
+            </div>
+            <div className="list-item">
+              {song.map((item) => {
+                return (
+                  <div key={item.id} className="music-list-item">
+                    <div className="number-td">{item.id}</div>
+                    <div className="title-td">{item.title}</div>
+                    <div className="artist-td">{item.owner.name}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PlayMusic;
