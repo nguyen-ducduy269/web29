a5a83f370313c077fbb8bcb362ed7e5fad09b07c
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-09-20 19:17:33 +0700

    .

--- web35/shopping cart/src/components/Nav.jsx
+++ web35/shopping cart/src/components/Nav.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import NavItem from "./NavItem";
 import NavDropdown from "./NavDropdown";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-      <NavItem>Home</NavItem>
+      <NavItem>
+        <Link
+          to="/"
+          style={{
+            color: "black",
+            textDecoration: "unset",
+          }}
+        >
+          Home
+        </Link>
+      </NavItem>
       <NavItem>About</NavItem>
       <NavDropdown />
     </ul>
