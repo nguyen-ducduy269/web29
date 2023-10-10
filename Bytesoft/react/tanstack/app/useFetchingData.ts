a3a375a56bdcf3e9b791280d7e2f1c584f6c9f56
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-10-10 17:10:41 +0700

    .

--- Bytesoft/react/tanstack/app/useFetchingData.ts
+++ Bytesoft/react/tanstack/app/useFetchingData.ts
@@ -1,9 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 
 export const useFetchingData = () =>
-  useQuery(["data"], async () => {
-    const res = await fetch("http://localhost:3001/data");
-    return res.json();
+  useQuery({
+    queryKey: ["data"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:3001/data");
+      return res.json();
+    },
   });
 
 export const useFetchingProjectData = () =>
