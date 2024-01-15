4a097a93a7abbf06c3a57cb36561b208bf030e0c
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2024-01-15 11:39:33 +0700

    .

--- Bytesoft/react/plant shop/app/_components/checkout/CheckOut.tsx
+++ Bytesoft/react/plant shop/app/_components/checkout/CheckOut.tsx
@@ -1,70 +1,208 @@
-import { Container } from "@/app/_style-components/home-page-css/Container";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const CheckOutPage = () => {
-  return (
-    <Container>
-      <div className="check-out">
-        <div className="contact">
-          <div className="form-email">
-            <label htmlFor="email">Contact</label>
-            <input type="email" placeholder="Email" />
-          </div>
+  const [data, setData]: any = useState([]);
+  const [discount, setDiscount] = useState("");
+  const [changePercent, setChangePercent] = useState(0);
+  const city = [
+    "An Giang",
+    "Hà Giang",
+    "Ninh Thuận",
+    "Bà Rịa – Vũng Tàu",
+    "Hà Nam",
+    "Phú Thọ",
+    "Bạc Liêu",
+    "Hà Nội",
+    "Phú Yên",
+    "Bắc Giang",
+    "Hà Tĩnh",
+    "Quảng Bình",
+    "Bắc Kạn",
+    "Hải Dương",
+    "Quảng Nam",
+    "Bắc Ninh",
+    "Hải Phòng",
+    "Quảng Ngãi",
+    "Bến Tre",
+    "Hậu Giang",
+    "Quảng Ninh",
+    "Bình Dương",
+    'Hòa Bình", "Quảng Trị',
+    "Bình Định",
+    "Thành phố Hồ Chí Minh",
+    "Sóc Trăng",
+    "Bình Phước",
+    "Hưng Yên",
+    "Sơn La",
+    "Bình Thuận",
+    "Khánh Hòa",
+    "Tây Ninh",
+    "Cà Mau",
+    "Kiên Giang",
+    "Thái Bình",
+    "Cao Bằng",
+    "Kon Tum",
+    "Thái Nguyên",
+    "Cần Thơ",
+    "Lai Châu",
+    "Thanh Hóa",
+    "Đà Nẵng",
+    "Lạng Sơn",
+    "Thừa Thiên Huế",
+    "Đắk Lắk",
+    "Lào Cai",
+    "Tiền Giang",
+    "Đắk Nông",
+    "Lâm Đồng",
+    "Trà Vinh",
+    "Điện Biên",
+    "Long An",
+    "Tuyên Quang",
+    "Đồng Nai",
+    "Nam Định",
+    "Vĩnh Long",
+    "Đồng Tháp",
+    "Nghệ An",
+    "Vĩnh Phúc",
+    "Gia Lai",
+    "Ninh Bình",
+    "Yên Bá",
+  ];
+  let finalTotalPrice = 0;
 
-          <select name="" id="">
-            <option value="">United Staté</option>
+  const fetchData = async (url: string) => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setData(users);
+  };
+
+  useEffect(() => {
+    fetchData("http://localhost:4001/card");
+  }, []);
+
+  for (let i = 0; i < data.length; i++) {
+    finalTotalPrice += data[i].total_price;
+  }
+
+  const applyDiscount = () => {
+    if (discount === "DiscountGift1") {
+      alert("Apply discount code successfully!");
+      setChangePercent(10);
+    } else if (discount === "DiscountGift2") {
+      alert("Apply discount code successfully!");
+      setChangePercent(20);
+    } else if (discount === "Gift1") {
+      alert("Apply discount code successfully!");
+      setChangePercent(3);
+    } else if (discount === "Gift2") {
+      alert("Apply discount code successfully!");
+      setChangePercent(5);
+    } else {
+      return false;
+    }
+  };
+
+  return (
+    <div className="check-out">
+      <div className="contact">
+        <div className="form-email">
+          <label htmlFor="email">Contact</label>
+          <input type="email" placeholder="Email" />
+        </div>
+
+        <select name="" id="">
+          <option value="">Viet Nam</option>
+        </select>
+
+        <div className="name">
+          <input type="text" placeholder="First name" />
+
+          <input type="text" placeholder="Last name" />
+        </div>
+
+        <input type="text" placeholder="Company (optional)" />
+
+        <input type="text" placeholder="Address" />
+
+        <input type="text" placeholder="Apartment, suite, etc. (optional)" />
+
+        <div className="place">
+          <input type="text" placeholder="Ward" />
+
+          <select name="" id="" placeholder="State">
+            {city.map((location: any) => {
+              return <option value="">{location}</option>;
+            })}
           </select>
 
-          <div className="name">
-            <input type="text" placeholder="First name" />
+          <input type="text" placeholder="ZIP Code" />
+        </div>
 
-            <input type="text" placeholder="Last name" />
+        <input type="text" placeholder="Phone (optional)" />
+
+        <div className="pay-now">Pay Now</div>
+      </div>
+
+      <div className="summary">
+        <div className="order">
+          {data.map((item: any) => {
+            return (
+              <div className="order-item">
+                <div className="image">
+                  <img src={item.image} alt="" />
+                </div>
+
+                <div className="detail">
+                  <div className="name">{item.name}</div>
+                  <div className="pot">{item.size}</div>
+                  <div className="quantity">Quantity: {item.quantity}</div>
+                </div>
+
+                <div className="total-price">${item.total_price}</div>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="discount-code">
+          <input
+            type="text"
+            placeholder="Discount code or gift card"
+            value={discount}
+            onChange={(e: any) => setDiscount(e.target.value)}
+          />
+
+          <button className="apply" onClick={() => applyDiscount()}>
+            Apply
+          </button>
+        </div>
+
+        <div className="final">
+          <div className="subtotal">
+            Subtotal <p>${finalTotalPrice}</p>
           </div>
 
-          <input type="text" placeholder="Company (optional)" />
-
-          <input type="text" placeholder="Address" />
-
-          <input type="text" placeholder="Apartment, suite, etc. (optional)" />
-
-          <div className="place">
-            <input type="text" placeholder="City" />
-
-            <select name="" id="" placeholder="State">
-              <option value="">Alabama</option>
-              <option value="">Alaska</option>
-              <option value="">American Samoa</option>
-              <option value="">Arizona</option>
-              <option value="">Arkansas</option>
-              <option value="">California</option>
-              <option value="">Colorado</option>
-              <option value="">Connecticut</option>
-              <option value="">Delaware</option>
-              <option value="">Micronesia</option>
-              <option value="">Florida</option>
-              <option value="">Georgia</option>
-              <option value="">Guam</option>
-              <option value="">Hawaii</option>
-              <option value="">Idaho</option>
-              <option value="">Illinois</option>
-              <option value="">Indiana</option>
-              <option value="">Iowa</option>
-              <option value="">Kansas</option>
-              <option value="">Kentucky</option>
-              <option value="">Louisiana</option>
-              <option value="">Maine</option>
-              <option value="">Marshall Islands</option>
-              <option value="">Maryland</option>
-              <option value="">Massachchusetts</option>
-            </select>
-
-            <input type="text" placeholder="ZIP Code" />
+          <div className="shipping">
+            Shipping Discount <p>{changePercent}%</p>
           </div>
 
-          <input type="text" placeholder="Phone (optional)" />
+          <div className="total">
+            Total{" "}
+            {discount !== "" ? (
+              <p>
+                $
+                {Math.round(
+                  (finalTotalPrice - finalTotalPrice * (changePercent / 100)) *
+                    100
+                ) / 100}
+              </p>
+            ) : (
+              <p>${finalTotalPrice}</p>
+            )}
+          </div>
         </div>
       </div>
-    </Container>
+    </div>
   );
 };
 
--- Bytesoft/react/plant shop/app/_style-components/checkout/CheckOutCss.tsx
+++ Bytesoft/react/plant shop/app/_style-components/checkout/CheckOutCss.tsx
@@ -23,10 +23,296 @@ export const CheckOutCss = styled.div`
   }
 
   .check-out {
+    display: flex;
+    gap: 30px;
     .contact {
+      padding: 50px 0px 50px 100px;
+      width: 50%;
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
       input,
       select {
-        padding-left: 10px;
+        padding: 20px 10px;
+        border-radius: 5px;
+        border: 0.5px solid #00000038;
+      }
+
+      .form-email {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        label {
+          font-size: 24px;
+          font-weight: 600;
+        }
+
+        input {
+          height: 48px;
+          border-radius: 5px;
+          border: 0.5px solid #00000038;
+        }
+      }
+
+      select {
+        width: 100%;
+        border-radius: 5px;
+        border: 0.5px solid #00000038;
+      }
+
+      .name {
+        display: flex;
+        gap: 20px;
+        width: 100%;
+        input {
+          width: 100%;
+        }
+      }
+
+      .place {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 20px;
+      }
+
+      .pay-now {
+        width: 100%;
+        padding: 20px 10px;
+        font-weight: 600;
+        font-size: 18px;
+        background-color: #00644b;
+        border: none;
+        border-radius: 5px;
+        color: white;
+        text-align: center;
+      }
+    }
+
+    .summary {
+      padding: 30px 50px;
+      background-color: rgba(250, 250, 250, 1);
+      width: 50%;
+      border-left: 0.5px solid #00000038;
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+      .order {
+        .order-item {
+          padding: 20px 0px 0px 0px;
+          display: flex;
+          gap: 10px;
+          position: relative;
+          justify-content: space-between;
+          .image {
+            width: 10%;
+            img {
+              width: 100%;
+              height: fit-content;
+              border: 0.5px solid #00000038;
+              border-radius: 5px;
+            }
+          }
+
+          .detail {
+            width: 75%;
+            display: flex;
+            flex-direction: column;
+            gap: 2px;
+            justify-content: center;
+            div {
+              font-size: 12px;
+            }
+
+            .name {
+              font-weight: 600;
+            }
+          }
+
+          .total-price {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+            font-size: 15px;
+            font-weight: 500;
+          }
+        }
+      }
+
+      .discount-code {
+        display: flex;
+        gap: 20px;
+        input {
+          width: 90%;
+          padding: 15px 10px;
+          border: 0.5px solid #00000038;
+          border-radius: 5px;
+        }
+
+        button {
+          padding: 15px 15px;
+          color: rgba(0, 0, 0, 0.56);
+          font-weight: 600;
+          border: 0.5px solid rgba(218, 218, 218, 1);
+          border-radius: 5px;
+          cursor: pointer;
+        }
+      }
+
+      .final {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        margin-bottom: 50px;
+        .subtotal,
+        .shipping {
+          display: flex;
+          width: 100%;
+          justify-content: space-between;
+          font-size: 14px;
+          p {
+            font-weight: 600;
+          }
+        }
+
+        .total {
+          display: flex;
+          width: 100%;
+          justify-content: space-between;
+          font-size: 16px;
+          font-weight: 600;
+        }
+      }
+    }
+  }
+
+  @media only screen and (min-width: 320px) {
+    .check-out {
+      flex-direction: column;
+      .contact {
+        width: 100%;
+        padding: 30px 10px;
+      }
+
+      .summary {
+        width: 100%;
+        padding: 0px 10px;
+        .order {
+          .order-item {
+            .image {
+              width: 25%;
+            }
+
+            .detail {
+              width: 55%;
+              div {
+                font-size: 10px;
+              }
+            }
+
+            .total-price {
+              font-size: 12px;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @media only screen and (min-width: 425px) {
+    .check-out {
+      flex-direction: column;
+      .contact {
+        width: 100%;
+        padding: 30px 10px;
+      }
+
+      .summary {
+        width: 100%;
+        padding: 0px 10px;
+        .order {
+          .order-item {
+            .image {
+              width: 20%;
+            }
+
+            .detail {
+              width: 60%;
+              div {
+                font-size: 12px;
+              }
+            }
+
+            .total-price {
+              font-size: 14px;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @media only screen and (min-width: 600px) {
+    .check-out {
+      flex-direction: column;
+      .contact {
+        width: 100%;
+        padding: 30px 10px;
+      }
+
+      .summary {
+        width: 100%;
+        padding: 0px 10px;
+        .order {
+          .order-item {
+            .image {
+              width: 15%;
+            }
+
+            .detail {
+              width: 65%;
+              div {
+                font-size: 12px;
+              }
+            }
+
+            .total-price {
+              font-size: 14px;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @media only screen and (min-width: 768px) {
+    .check-out {
+      flex-direction: row;
+      .contact {
+        width: 50%;
+        padding: 50px 0px 50px 100px;
+      }
+
+      .summary {
+        width: 50%;
+        padding: 30px 50px;
+        .order {
+          .order-item {
+            .image {
+              width: 10%;
+            }
+
+            .detail {
+              width: 75%;
+              div {
+                font-size: 12px;
+              }
+            }
+
+            .total-price {
+              font-size: 15px;
+            }
+          }
+        }
       }
     }
   }
