860cc7c1c293c525eaa296cc064843735f84007d
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2024-02-29 15:14:51 +0700

    .

--- Bytesoft/react/wagmi-project/src/app/page.tsx
+++ Bytesoft/react/wagmi-project/src/app/page.tsx
@@ -1,47 +1,26 @@
 "use client";
 
-import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { config } from "@/config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider, useAccount } from "wagmi";
+import { Account } from "./account";
+import { WalletOptions } from "./wallet-option";
+
+const queryClient = new QueryClient();
+
+function ConnectWallet() {
+  const { isConnected } = useAccount();
+  if (isConnected) return <Account />;
+  return <WalletOptions />;
+}
 
 function App() {
-  const account = useAccount();
-  const { connectors, connect, status, error } = useConnect();
-  const { disconnect } = useDisconnect();
-
   return (
-    <>
-      <div>
-        <h2>Account</h2>
-
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
-        </div>
-
-        {account.status === "connected" && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
-    </>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectWallet />
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
--- Bytesoft/react/wagmi-project/src/config.ts
+++ Bytesoft/react/wagmi-project/src/config.ts
@@ -2,12 +2,14 @@ import { createConfig, http } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
 import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
 
+const projectId = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+
 export const config = createConfig({
   chains: [mainnet, sepolia],
   connectors: [
     injected(),
     coinbaseWallet({ appName: "Create Wagmi" }),
-    walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID || "" }),
+    walletConnect({ projectId }),
   ],
   ssr: true,
   transports: {
