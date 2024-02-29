e0ba243c9f7d33aa825bd5518aa3f1962134f518
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2024-02-29 17:22:42 +0700

    Update account.tsx

--- Bytesoft/react/wagmi-project/src/app/account.tsx
+++ Bytesoft/react/wagmi-project/src/app/account.tsx
@@ -10,7 +10,10 @@ export function Account() {
     <div>
       {ensAvatar && <img alt="ENS Avatar" src={ensAvatar} />}
       {address && (
-        <div>Wallet ID: {ensName ? `${ensName} (${address})` : address}</div>
+        <div>
+          <div>Wallet ID: {ensName ? `${ensName} (${address})` : address}</div>
+          <div>Wallet Value: </div>
+        </div>
       )}
       <button onClick={() => disconnect()}>Disconnect</button>
     </div>
