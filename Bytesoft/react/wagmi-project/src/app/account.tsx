a15bbdfa22e9aa4cfd4d61813bffcc6c9717b9a4
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2024-03-05 11:20:10 +0700

    Update account.tsx

--- Bytesoft/react/wagmi-project/src/app/account.tsx
+++ Bytesoft/react/wagmi-project/src/app/account.tsx
@@ -1,29 +1,10 @@
-import { erc20Abi } from "viem";
-import {
-  useAccount,
-  useDisconnect,
-  useEnsAvatar,
-  useEnsName,
-  useReadContracts,
-} from "wagmi";
+import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from "wagmi";
 
 export function Account() {
   const { address } = useAccount();
   const { disconnect } = useDisconnect();
   const { data: ensName } = useEnsName({ address });
   const { data: ensAvatar } = useEnsAvatar({ name: ensName! });
-
-  const result = useReadContracts({
-    contracts: [
-      {
-        address: "0xBda61E0D09776FcE75cDF3971ED6213F51000b7D",
-        abi: erc20Abi,
-        functionName: "balanceOf",
-      },
-    ],
-  });
-
-  console.log(result);
 
   return (
     <div>
