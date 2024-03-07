d69c47f75b5fdb14e8d370e3ba947c71c754ec16
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2024-03-07 15:53:11 +0700

    Update account.tsx

--- Bytesoft/react/wagmi-project/src/app/account.tsx
+++ Bytesoft/react/wagmi-project/src/app/account.tsx
@@ -1,10 +1,28 @@
-import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from "wagmi";
+import {
+  useAccount,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+  useReadContracts,
+} from "wagmi";
 
 export function Account() {
   const { address } = useAccount();
   const { disconnect } = useDisconnect();
   const { data: ensName } = useEnsName({ address });
   const { data: ensAvatar } = useEnsAvatar({ name: ensName! });
+
+  const IP = "0xBda61E0D09776FcE75cDF3971ED6213F51000b7D";
+
+  const result = useReadContracts({
+    allowFailure: false,
+    contracts: [
+      {
+        address: IP,
+      },
+    ],
+  });
+  console.log("result", result);
 
   return (
     <div>
