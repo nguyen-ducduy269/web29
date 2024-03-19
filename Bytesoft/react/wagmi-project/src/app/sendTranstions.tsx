617dc3cfcdd6fbb3866033bd8e209f2e2ee2fea0
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2024-03-19 11:22:01 +0700

    .

---
+++ Bytesoft/react/wagmi-project/src/app/sendTranstions.tsx
@@ -0,0 +1,293 @@
+import * as React from "react";
+import { parseEther } from "viem";
+import {
+  useSendTransaction,
+  useWaitForTransactionReceipt,
+  type BaseError,
+} from "wagmi";
+
+export function SendTransaction() {
+  const {
+    data: hash,
+    error,
+    isPending,
+    sendTransaction,
+  } = useSendTransaction();
+
+  async function submit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const to = formData.get("address") as `0x${string}`;
+    const value = formData.get("value") as string;
+    sendTransaction({ to, value: parseEther(value) });
+  }
+
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash,
+    });
+
+  const abi = [
+    [
+      {
+        constant: true,
+        inputs: [],
+        name: "name",
+        outputs: [
+          {
+            name: "",
+            type: "string",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        constant: false,
+        inputs: [
+          {
+            name: "_spender",
+            type: "address",
+          },
+          {
+            name: "_value",
+            type: "uint256",
+          },
+        ],
+        name: "approve",
+        outputs: [
+          {
+            name: "",
+            type: "bool",
+          },
+        ],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: "totalSupply",
+        outputs: [
+          {
+            name: "",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        constant: false,
+        inputs: [
+          {
+            name: "_from",
+            type: "address",
+          },
+          {
+            name: "_to",
+            type: "address",
+          },
+          {
+            name: "_value",
+            type: "uint256",
+          },
+        ],
+        name: "transferFrom",
+        outputs: [
+          {
+            name: "",
+            type: "bool",
+          },
+        ],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: "decimals",
+        outputs: [
+          {
+            name: "",
+            type: "uint8",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        constant: true,
+        inputs: [
+          {
+            name: "_owner",
+            type: "address",
+          },
+        ],
+        name: "balanceOf",
+        outputs: [
+          {
+            name: "balance",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: "symbol",
+        outputs: [
+          {
+            name: "",
+            type: "string",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        constant: false,
+        inputs: [
+          {
+            name: "_to",
+            type: "address",
+          },
+          {
+            name: "_value",
+            type: "uint256",
+          },
+        ],
+        name: "transfer",
+        outputs: [
+          {
+            name: "",
+            type: "bool",
+          },
+        ],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        constant: true,
+        inputs: [
+          {
+            name: "_owner",
+            type: "address",
+          },
+          {
+            name: "_spender",
+            type: "address",
+          },
+        ],
+        name: "allowance",
+        outputs: [
+          {
+            name: "",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        payable: true,
+        stateMutability: "payable",
+        type: "fallback",
+      },
+      {
+        anonymous: false,
+        inputs: [
+          {
+            indexed: true,
+            name: "owner",
+            type: "address",
+          },
+          {
+            indexed: true,
+            name: "spender",
+            type: "address",
+          },
+          {
+            indexed: false,
+            name: "value",
+            type: "uint256",
+          },
+        ],
+        name: "Approval",
+        type: "event",
+      },
+      {
+        anonymous: false,
+        inputs: [
+          {
+            indexed: true,
+            name: "from",
+            type: "address",
+          },
+          {
+            indexed: true,
+            name: "to",
+            type: "address",
+          },
+          {
+            indexed: false,
+            name: "value",
+            type: "uint256",
+          },
+        ],
+        name: "Transfer",
+        type: "event",
+      },
+    ],
+  ];
+
+  return (
+    <form onSubmit={submit}>
+      <input
+        name="address"
+        placeholder="0x......"
+        required
+        style={{ width: "350px", height: "25px", paddingLeft: "10px" }}
+      />
+      <input
+        name="value"
+        placeholder="0.05"
+        required
+        style={{
+          width: "100px",
+          height: "25px",
+          marginLeft: 10,
+          paddingLeft: "10px",
+        }}
+      />
+      <button
+        disabled={isPending}
+        type="submit"
+        style={{
+          width: "150px",
+          height: "33px",
+          marginLeft: 10,
+        }}
+      >
+        {isPending ? "Confirming..." : "Send"}
+      </button>
+      {hash && <div>Transaction Hash: {hash}</div>}
+      {isConfirming && <div>Waiting for confirmation...</div>}
+      {isConfirmed && <div>Transaction confirmed.</div>}
+      {error && (
+        <div>Error: {(error as BaseError).shortMessage || error.message}</div>
+      )}
+    </form>
+  );
+}
--- Bytesoft/react/wagmi-project/src/config.ts
+++ Bytesoft/react/wagmi-project/src/config.ts
@@ -1,5 +1,8 @@
 import { createConfig, http } from "wagmi";
 import { avalancheFuji } from "wagmi/chains";
+import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
+
+const projectId = "0xBda61E0D09776FcE75cDF3971ED6213F51000b7D";
 
 declare module "wagmi" {
   interface Register {
@@ -9,6 +12,7 @@ declare module "wagmi" {
 
 export const config = createConfig({
   chains: [avalancheFuji],
+  connectors: [injected(), walletConnect({ projectId }), metaMask(), safe()],
   transports: {
     [avalancheFuji.id]: http(),
   },
